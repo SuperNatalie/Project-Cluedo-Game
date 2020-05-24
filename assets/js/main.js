76b60f6a813f77d270f1e6e27ca843a0464c0475
Natalie <natalie@MacBook-Pro.local>
2020-05-24 14:13:06 +0200

    killer pic bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -116,6 +116,7 @@ function selectRandom() {
 
 function pickMystery() {
 selectRandom();
+var killerPic = document.getElementById("killerpic");
 answer.innerHTML = "The secret envelope has been filled! Whats's your guess..?";
 mystery = [randomSuspect, randomWeapon, randomRoom];
 killerPic.innerHTML =  "<img src='./assets/img/secret_envelope.png'>";
